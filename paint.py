d5de5e7aac741bacf632c651f8a4cf8274352855
Gustavo Santos <gfdsantos@inf.ufpel.edu.br>
2017-02-22 17:09:33 -0300

    Versão final

--- paint.py
+++ paint.py
@@ -4,99 +4,39 @@
 import pygame, random, pyaudio,math
 import struct
 import thread, threading
-tones=[ 
-[ 440  , 493  , 523  , 587  , 659  , 739  , 783  , 783  , 739  , 659  , 587  , 523  , 493  , 440   ],
-[ 493  , 523  , 587  , 659  , 739  , 783  , 783  , 739  , 659  , 587  , 523  , 493  , 440  , 440   ],
-[ 523  , 587  , 659  , 739  , 783  , 783  , 739  , 659  , 587  , 523  , 493  , 440  , 440  , 493   ],
-[ 587  , 659  , 739  , 783  , 783  , 739  , 659  , 587  , 523  , 493  , 440  , 440  , 493  , 523   ],
-[ 659  , 739  , 783  , 783  , 739  , 659  , 587  , 523  , 493  , 440  , 440  , 493  , 523  , 587   ],
-[ 739  , 783  , 783  , 739  , 659  , 587  , 523  , 493  , 440  , 440  , 493  , 523  , 587  , 659   ],
-[ 783  , 783  , 739  , 659  , 587  , 523  , 493  , 440  , 440  , 493  , 523  , 587  , 659  , 739   ],
-[ 783  , 739  , 659  , 587  , 523  , 493  , 440  , 440  , 493  , 523  , 587  , 659  , 739  , 783   ],
-[ 739  , 659  , 587  , 523  , 493  , 440  , 440  , 493  , 523  , 587  , 659  , 739  , 783  , 783   ],
-[ 659  , 587  , 523  , 493  , 440  , 440  , 493  , 523  , 587  , 659  , 739  , 783  , 783  , 739   ],
-[ 587  , 523  , 493  , 440  , 440  , 493  , 523  , 587  , 659  , 739  , 783  , 783  , 739  , 659   ],
-[ 523  , 493  , 440  , 440  , 493  , 523  , 587  , 659  , 739  , 783  , 783  , 739  , 659  , 587   ],
-[ 493  , 440  , 440  , 493  , 523  , 587  , 659  , 739  , 783  , 783  , 739  , 659  , 587  , 523  ],
-[ 440  , 440  , 493  , 523  , 587  , 659  , 739  , 783  , 783  , 739  , 659  , 587  , 523  , 493   ]
-]
-tones_=[ 
-[ "A"  , "B"  , "C"  , "D"  , "E"  , "F"  , "G"  , "G"  , "F"  , "E"  , "D"  , "C"  , "B"  , "A"   ],
-[ "B"  , "C"  , "D"  , "E"  , "F"  , "G"  , "G"  , "F"  , "E"  , "D"  , "C"  , "B"  , "A"  , "A"   ],
-[ "C"  , "D"  , "E"  , "F"  , "G"  , "G"  , "F"  , "E"  , "D"  , "C"  , "B"  , "A"  , "A"  , "B"   ],
-[ "D"  , "E"  , "F"  , "G"  , "G"  , "F"  , "E"  , "D"  , "C"  , "B"  , "A"  , "A"  , "B"  , "C"   ],
-[ "E"  , "F"  , "G"  , "G"  , "F"  , "E"  , "D"  , "C"  , "B"  , "A"  , "A"  , "B"  , "C"  , "D"   ],
-[ "F"  , "G"  , "G"  , "F"  , "E"  , "D"  , "C"  , "B"  , "A"  , "A"  , "B"  , "C"  , "D"  , "E"   ],
-[ "G"  , "G"  , "F"  , "E"  , "D"  , "C"  , "B"  , "A"  , "A"  , "B"  , "C"  , "D"  , "E"  , "F"   ],
-[ "G"  , "F"  , "E"  , "D"  , "C"  , "B"  , "A"  , "A"  , "B"  , "C"  , "D"  , "E"  , "F"  , "G"   ],
-[ "F"  , "E"  , "D"  , "C"  , "B"  , "A"  , "A"  , "B"  , "C"  , "D"  , "E"  , "F"  , "G"  , "G"   ],
-[ "E"  , "D"  , "C"  , "B"  , "A"  , "A"  , "B"  , "C"  , "D"  , "E"  , "F"  , "G"  , "G"  , "F"   ],
-[ "D"  , "C"  , "B"  , "A"  , "A"  , "B"  , "C"  , "D"  , "E"  , "F"  , "G"  , "G"  , "F"  , "E"   ],
-[ "C"  , "B"  , "A"  , "A"  , "B"  , "C"  , "D"  , "E"  , "F"  , "G"  , "G"  , "F"  , "E"  , "D"   ],
-[ "B"  , "A"  , "A"  , "B"  , "C"  , "D"  , "E"  , "F"  , "G"  , "G"  , "F"  , "E"  , "D"  , "C"  ],
-[ "A"  , "A"  , "B"  , "C"  , "D"  , "E"  , "F"  , "G"  , "G"  , "F"  , "E"  , "D"  , "C"  , "B"   ]
-]
-tones1 = {
-        'A': 440,
-        'A#': 466,
-        'B': 493,
-        'C': 523,
-        'C#': 554,
-        'D': 587,
-        'D#': 622,
-        'E': 659,
-        'F': 698,
-        'F#': 739,
-        'G': 783,
-        'G#': 830,
-        'a': 880,
-        'a#': 932,
-        'b': 987,
-        'c': 1046,
-        'c#': 1108,
-        'd': 1174,
-        'd#': 1244,
-        'e': 1318,
-        'f': 1396,
-        'f#': 1479,
-        'g': 1567,
-        'g#': 1661
-    }
-escala_menor=[
-["C","D","D#","F","G","G#","B"],
-["C#","D#","E","F#","G#","A","C"],
-["D","E","F","G","A","A#","C#"],
-["D#","F","F#","G#","A#","B","D"],
-["E","F#","G","A","B","C","D#"],
-["F","G","G#","A#","C","C#","E"],
-["F#","G#","A","B","C#","D","F"],
-["G","A","A#","C","D","D#","F#"],
-["G#","A#","B","C#","D#","E","G"],
-["A","B","C","D","E","F","G#"],
-["A#","C","C#","D#","F","F#","A"],
-["B","C#","D","E","F#","G","A#"]
-]
-escala_pentatonica_maior={
-"C":["C","D","E","G","A"],
-"C#":["C#","D#","F","G#","A#"],
-"D":["D","E","F#","A","B"],
-"D#": ["D#","F","G","A#","C"],
-"E":["E","F#","G#","B","C#"],
-"F": ["F","G","A","C","D"],
-"F#": ["F#","G#","A#","C#","D"],
-"G":["G","A","B","D","E"],
-"G#":["G#","A#","C","D#","F"],
-"A":["A","B","C#","E","F#"],
-"A#":["A#","C","D","F","G"],
-"B":["B","C#","D#","F#","G#"]
-}
-tones3 = [ 440, 493,
+
+tones = [ 440, 
+        493,
          523,
          587,
          659,
         698,
         783
 ]
+tones1 = [  523,
+        554,
+        587,
+        622,
+        659,
+        698,
+        739,
+        783,
+        830,
+        440,
+        466,
+        493,
+        1046,
+        1108,
+        1174,
+        1244,
+        1318,
+         1396,
+         1479,
+         1567,
+         1661,        
+        880,
+         932,
+         987]
 
 
 global cont_escala
@@ -115,7 +55,14 @@ lock =  threading.Lock()
 fs = 5000
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32,channels=1,rate=fs,output=True)
-
+def carregarPiano():
+    n = 12
+    piano = []
+    for i in range(1,n):
+        nome ="sound/"+ i+".wav"
+        sounds.append(pygame.mixer.Sound(nome))
+    return piano
+    
 def daemon(w,j):
     global desenhando, cont_escala,escala_atual
     a = 20
@@ -130,19 +77,13 @@ def daemon(w,j):
         if nota > -1:            
             if (desenhando == True):
                 a=( posAtual[0]*1.0/w*1.0 ) *2
-                          
-                play_tone(tones3[nota], a, 0.1, fs, stream)
-                print "tocar desenhando "
-                print nota
+                play_tone(tones1[nota], a, 0.1, fs, stream)
                 
-            #else:
-           #     nota =0  
-       # lock.release( 
-    
+        
 
 def notas_Div(height):
     print height
-    n_divs = 7
+    n_divs = len(tones1) - 1# 10
     divs = height/n_divs
     areas =[]
     area = 0;
@@ -155,11 +96,9 @@ def play_tone(frequency, amplitude, duration, fs, stream):
     N = int(fs / frequency)
     T = int(frequency * duration)  # repeat for T cycles
     dt = 1.0/ fs
-   
     # 1 cycle
     tone = (amplitude * math.sin(2 * math.pi * frequency * n * dt)
             for n in xrange(N))
-    # todo: get the format from the stream; this assumes Float32
     data = ''.join(struct.pack('f', samp) for samp in tone)    
     for n in xrange(T):
         stream.write(data)
@@ -192,19 +131,46 @@ def partitura(height, posY):
     divs = notas_Div(height)
     global nota
     i=0
-
-    while posY>divs[i]:
-        print divs[i],posY
-        i+=1
-        
-    nota =i-1
+    
+    while (i< len(divs))and(posY>divs[i]) :
+        i+=1        
+    if(i<len(divs)):
+        nota = (i-1)
+    else: 
+        nota = len(divs)-1
+    print "pos: ", posY,len(divs)
+    if(posY<100):
+        print "7"
+    elif(posY<120):
+        print "1"
+    elif(posY<140):
+        print "1"
+    elif(posY<240):
+        print "8"
+    elif(posY<260):
+        print "7"
+    elif(posY<360):
+        print "6"
+    elif(posY<380):
+        print "5"
+    elif(posY<478):
+        print "4"
+    elif(posY<490):
+        print "3"
+    elif(posY<600):
+        print "2"
+    elif(posY<640):
+        print "1"
+    else:
+        print "0"
     print " nota ",nota    
         
 def main():
-    w = 800
-    h=600
+    w =  1336
+    h=  720
     pygame.init()
-    tela = pygame.display.set_mode((w,h))
+    tela = pygame.display.set_mode((w,h),pygame.FULLSCREEN)#pygame.display.set_mode((w,h))
+    #pygame.display.set_mode((640,480),pygame.FULLSCREEN)
     pygame.display.set_caption('Aquarela Musical')
     tela.fill([0, 0, 0])
     borracha = (0, 0 ,0)
@@ -218,22 +184,18 @@ def main():
     raio_borracha = 20
     raio = 10
     
-    '''
     clave = pygame.image.load('path3.png')
-    clave = pygame.transform.scale(clave, (100, h))
+    clave = pygame.transform.scale(clave, (w/5, h))
     clave_quad = pygame.draw.rect(tela, (0,0,0),(0,0,10,10) )
     
     linhas = pygame.image.load('g4518.png')
     linhas = pygame.transform.scale(linhas, (w, h))
     linhas_quad = pygame.draw.rect(tela, (0,0,0),(0,0,w,h) )
     
-    musica_fundo = pygame.mixer.music.load('OdetoJoy_Beethoven.mp3')
-    
-    pygame.mixer.music.play(1,0)
-    '''
+    # musica_fundo = pygame.mixer.Sound('./OdetoJoy_Beethoven.mp3')
+    # musica_fundo.play()
     lock.acquire()
     thread.start_new_thread( daemon, (w,0) )
-    
     global posAtual
     try:
         while True:
@@ -259,6 +221,8 @@ def main():
                     if desenhando == True:
                         #global cor
                     	pygame.draw.circle(tela, cor, last_pos, raio)
+                elif e.key == 102:
+                  raise StopIteration
             if e.type == pygame.KEYUP:
                 #cor = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))#random.choice(cores)
                 global desenhando
@@ -286,6 +250,12 @@ def main():
                 desenhando = True
                 
             if e.type == pygame.MOUSEBUTTONUP:
+                tela.fill([0, 0, 0])
+                pygame.display.flip()
+                tela.blit(linhas,linhas_quad)
+                tela.blit(clave,clave_quad)
+            
+                
                 posAtual = e.pos
                # cor = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))#random.choice(cores)
                 global desenhando
@@ -304,8 +274,8 @@ def main():
                 posAtual =  last_pos
                 
             pygame.display.flip()
-            #tela.blit(linhas,linhas_quad)
-            #tela.blit(clave,clave_quad)
+            tela.blit(linhas,linhas_quad)
+            tela.blit(clave,clave_quad)
             
     except StopIteration:
         pass
